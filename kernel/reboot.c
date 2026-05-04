c951f335f77d9b4fbae777101eb507a8c906e9a5
tem423 <3217752896@qq.com>
2026-05-04 12:15:23 +0800

    更新 reboot.c

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -241,14 +241,15 @@ void migrate_to_reboot_cpu(void)
  */
 void kernel_restart(char *cmd)
 {
-	kernel_restart_prepare(cmd);
-	migrate_to_reboot_cpu();
-	syscore_shutdown();
+	
 	if (!cmd)
 		pr_emerg("Restarting system\n");
 	else
 		pr_emerg("Restarting system with command '%s'\n", cmd);
 	kmsg_dump(KMSG_DUMP_RESTART);
+	kernel_restart_prepare(cmd);
+	migrate_to_reboot_cpu();
+	syscore_shutdown();
 	machine_restart(cmd);
 }
 EXPORT_SYMBOL_GPL(kernel_restart);
