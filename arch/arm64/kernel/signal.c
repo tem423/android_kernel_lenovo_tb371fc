b73f3901eb71f1e617cf980ebb4362b0bab79583
tem423 <3217752896@qq.com>
2026-05-09 23:02:24 +0800

    更新 signal.c

--- arch/arm64/kernel/signal.c
+++ arch/arm64/kernel/signal.c
@@ -749,12 +749,7 @@ static void setup_return(struct pt_regs *regs, struct k_sigaction *ka,
 	if (ka->sa.sa_flags & SA_RESTORER)
 		sigtramp = ka->sa.sa_restorer;
 	else
-		#ifdef vdso_offset_sigtramp
-sigtramp = VDSO_SYMBOL(current->mm->context.vdso, sigtramp);
-#else
-sigtramp = NULL; 
-#warning "VDSO sigtramp offset missing, using fallback"
-#endif
+        sigtramp = VDSO_SYMBOL(current->mm->context.vdso, sigtramp);
 
 	regs->regs[30] = (unsigned long)sigtramp;
 }
