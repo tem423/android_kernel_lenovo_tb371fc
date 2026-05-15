e06be916db588c8a44abc66c542327912c35c890
tem423 <3217752896@qq.com>
2026-05-15 23:29:45 +0800

    更新 mm8013.c

--- drivers/power/supply/qcom/mm8013.c
+++ drivers/power/supply/qcom/mm8013.c
@@ -19,17 +19,16 @@
 #define REG_BLOCKDATAOFFSET                 0x3e
 #define REG_BLOCKDATA                       0x40
 
-
-bool has_8013 = false;
 struct mm8013_chip {
     struct i2c_client *client;
-    struct power_supply_desc battery;
+    struct power_supply *psy;
+    char serial_num[8];      /* 存储电池ID字符串 */
 };
 
+static struct mm8013_chip *global_chip;
 
-struct mm8013_chip *chip;
+static int battery_id = 1;
 
-int battery_id=1;
 static int mm8013_write_reg(struct i2c_client *client, u8 reg, u16 value)
 {
     int ret = i2c_smbus_write_word_data(client, reg, value);
@@ -94,9 +93,8 @@ static int mm8013_temperature(struct mm8013_chip *chip, int *val)
 
     if (temp < 0)
     {
-	*val = 2550;
-
-	return 0;
+        *val = 2550;
+        return 0;
     }
     *val = temp - 2731;
 
@@ -105,158 +103,166 @@ static int mm8013_temperature(struct mm8013_chip *chip, int *val)
 
 static int mm8013_checkdevice(struct mm8013_chip *chip)
 {
-	int ret;
-    	ret = mm8013_write_reg(chip->client, 0x0, 0x0008);
-    	if (ret < 0)
-	{
-		return ret;
-	}
+    int ret;
+    ret = mm8013_write_reg(chip->client, 0x0, 0x0008);
+    if (ret < 0)
+    {
+        return ret;
+    }
 
-    	ret = mm8013_read_reg(chip->client, 0x0);
-    	if (ret < 0)
-	{
-		return ret;
-	}
+    ret = mm8013_read_reg(chip->client, 0x0);
+    if (ret < 0)
+    {
+        return ret;
+    }
 
-	if( ret== 0x0102)
-	{
-		battery_id=2;
-	}
-	else if( ret== 0x0101)
-	{
-		battery_id=1;
-	}
-	else
-	{
-		printk("====unknow_battery==== \r\n");
-   		return -1;
+    if (ret == 0x0102)
+    {
+        battery_id = 2;
+        strcpy(chip->serial_num, "2");
+    }
+    else if (ret == 0x0101)
+    {
+        battery_id = 1;
+        strcpy(chip->serial_num, "1");
+    }
+    else
+    {
+        printk("====unknown_battery==== \r\n");
+        return -1;
+    }
+    printk("8013 0x0008:[0x%x]  battery_id:%d \r\n", ret, battery_id);
 
-	}
-	printk("8013 0x0008:[0x%x]  battery_id:%d \r\n",ret,battery_id);
-
-        return 0;
+    return 0;
 }
 
+/* 修改1：使用标准属性名 */
 static enum power_supply_property mm8013_battery_props[] = {
-    POWER_SUPPLY_PROP_batt_cap,
+    POWER_SUPPLY_PROP_CAPACITY,        /* 原 POWER_SUPPLY_PROP_batt_cap */
     POWER_SUPPLY_PROP_VOLTAGE_NOW,
     POWER_SUPPLY_PROP_CURRENT_NOW,
     POWER_SUPPLY_PROP_TEMP,
-    POWER_SUPPLY_PROP_battery_id,
+    POWER_SUPPLY_PROP_SERIAL_NUMBER,   /* 原 POWER_SUPPLY_PROP_battery_id */
 };
-
-
 
 static int mm8013_get_property(struct power_supply *psy,
                     enum power_supply_property psp,
                     union power_supply_propval *val)
 {
-    int ret  = 0;
+    struct mm8013_chip *chip = power_supply_get_drvdata(psy);
+    int ret = 0;
 
+    if (!chip)
+        return -ENODEV;
 
     switch (psp) {
-    case POWER_SUPPLY_PROP_batt_cap:
-	ret = mm8013_soc(chip, &(val->intval));
-	break;
+    /* 修改2：使用对应的标准属性名 */
+    case POWER_SUPPLY_PROP_CAPACITY:   /* 原 POWER_SUPPLY_PROP_batt_cap */
+        ret = mm8013_soc(chip, &(val->intval));
+        break;
     case POWER_SUPPLY_PROP_VOLTAGE_NOW:
-	ret = mm8013_voltage(chip, &(val->intval));
-	break;
+        ret = mm8013_voltage(chip, &(val->intval));
+        break;
     case POWER_SUPPLY_PROP_CURRENT_NOW:
-	ret = mm8013_current(chip, &(val->intval));
-	break;
+        ret = mm8013_current(chip, &(val->intval));
+        break;
     case POWER_SUPPLY_PROP_TEMP:
-	ret = mm8013_temperature(chip, &(val->intval));
-	break;
-     case POWER_SUPPLY_PROP_battery_id:
-	val->intval = battery_id;
-	break;
+        ret = mm8013_temperature(chip, &(val->intval));
+        break;
+    /* 修改3：SERIAL_NUMBER 使用 strval 返回字符串 */
+    case POWER_SUPPLY_PROP_SERIAL_NUMBER:
+        val->strval = chip->serial_num;
+        break;
     default:
-	return -EINVAL;
+        return -EINVAL;
     }
 
     return ret;
 }
 
-
 static const struct power_supply_desc mm8013_desc = {
-	.name			= "mm8013",
-	.type			= POWER_SUPPLY_TYPE_BATTERY,
-	.properties		= mm8013_battery_props,
-	.num_properties		= 5,
-	.get_property		= mm8013_get_property,
-	.set_property		= NULL,
-	.property_is_writeable	= NULL,
+    .name           = "mm8013",
+    .type           = POWER_SUPPLY_TYPE_BATTERY,
+    .properties     = mm8013_battery_props,
+    .num_properties = ARRAY_SIZE(mm8013_battery_props),
+    .get_property   = mm8013_get_property,
+    .set_property   = NULL,
+    .property_is_writeable = NULL,
 };
 
-
-
-
 static int mm8013_probe(struct i2c_client *client,
-			const struct i2c_device_id *id)
+            const struct i2c_device_id *id)
 {
+    struct device *cdev = &client->dev;
+    struct mm8013_chip *chip;
+    struct power_supply_config psy_cfg = {};
+    int ret = 0;
 
+    if (!i2c_check_functionality(client->adapter, I2C_FUNC_SMBUS_WORD_DATA)) {
+        printk("smbus data not supported!\n");
+        return -EIO;
+    }
 
-	struct device *cdev = &client->dev;
-	int ret = 0;
+    chip = devm_kzalloc(cdev, sizeof(struct mm8013_chip), GFP_KERNEL);
+    if (!chip) {
+        printk("can't alloc mm8013_chip\n");
+        return -ENOMEM;
+    }
 
-	if (!i2c_check_functionality(client->adapter,I2C_FUNC_SMBUS_WORD_DATA)) {
-		printk("smbus data not supported!\n");
-		return -EIO;
-	}
+    chip->client = client;
+    i2c_set_clientdata(client, chip);
 
-	chip = devm_kzalloc(cdev, sizeof(struct mm8013_chip), GFP_KERNEL);
-	if (!chip) {
-		printk("can't alloc fusb301_chip\n");
-		return -ENOMEM;
-	}
+    ret = mm8013_checkdevice(chip);
+    if (ret) {
+        dev_err(&client->dev, "failed to access platform, no mm8013!\n");
+        return -EPROBE_DEFER;
+    }
 
-	chip->client = client;
+    psy_cfg.drv_data = chip;
+    chip->psy = power_supply_register(&client->dev, &mm8013_desc, &psy_cfg);
+    if (IS_ERR(chip->psy)) {
+        dev_err(&client->dev, "Failed to register power supply\n");
+        return PTR_ERR(chip->psy);
+    }
 
-	i2c_set_clientdata(client, chip);
-
-	ret = mm8013_checkdevice(chip);
-	if (ret)
-	{
-		dev_err(&client->dev, "================failed to access platform don't has mm8013!\n");
-		return -EPROBE_DEFER;;
-	}
-        power_supply_register(&client->dev, &mm8013_desc,NULL);
-	printk("mm8013-init down platform has mm8013!\n");
-
-
-	return 0;
+    printk("mm8013-init down platform has mm8013!\n");
+    return 0;
 }
 
 static const struct i2c_device_id mm8013_id_table[] = {
-	{"mm8013", 0},
-	{},
+    {"mm8013", 0},
+    {},
 };
 MODULE_DEVICE_TABLE(i2c, mm8013_id_table);
 
 static struct of_device_id mm8013_match_table[] = {
-	{ .compatible = "qcom,mm8013",},
-	{ },
+    { .compatible = "qcom,mm8013",},
+    { },
 };
 
 static struct i2c_driver mm8013_i2c_driver = {
-	.driver = {
-		.name = "mm8013",
-		.owner = THIS_MODULE,
-		.of_match_table = mm8013_match_table,
-	},
-	.probe = mm8013_probe,
-	.id_table = mm8013_id_table,
+    .driver = {
+        .name = "mm8013",
+        .owner = THIS_MODULE,
+        .of_match_table = mm8013_match_table,
+    },
+    .probe = mm8013_probe,
+    .id_table = mm8013_id_table,
 };
+
 static __init int mm8013_i2c_init(void)
 {
-	return i2c_add_driver(&mm8013_i2c_driver);
+    return i2c_add_driver(&mm8013_i2c_driver);
 }
+
 static __exit void mm8013_i2c_exit(void)
 {
-	i2c_del_driver(&mm8013_i2c_driver);
+    i2c_del_driver(&mm8013_i2c_driver);
 }
+
 fs_initcall(mm8013_i2c_init);
 module_exit(mm8013_i2c_exit);
+
 MODULE_AUTHOR("leon@lge.com");
 MODULE_DESCRIPTION("I2C bus driver for mm8013X gauge");
 MODULE_LICENSE("GPL v2");
