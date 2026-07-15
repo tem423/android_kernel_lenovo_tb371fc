d15f33d1e7216d80d0bac48e52b544e7f9f9da8e
tem423 <3217752896@qq.com>
2026-07-15 12:15:43 +0800

    Fix conflict markers in ext4/readpage.c

--- fs/ext4/readpage.c
+++ fs/ext4/readpage.c
@@ -160,10 +160,6 @@ static bool bio_post_read_required(struct bio *bio)
  */
 static void mpage_end_io(struct bio *bio)
 {
-<<<<<<< HEAD
-
-=======
->>>>>>> upstream/android16-base
 	if (bio_post_read_required(bio)) {
 		struct bio_post_read_ctx *ctx = bio->bi_private;
 
