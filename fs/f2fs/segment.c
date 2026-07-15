caf3947cc636ef1bd5039125b0764462ed750f44
tem423 <3217752896@qq.com>
2026-07-15 13:32:26 +0800

    Fix conflict in f2fs/segment.c

--- fs/f2fs/segment.c
+++ fs/f2fs/segment.c
@@ -1692,8 +1692,7 @@ static int issue_discard_thread(void *data)
 	set_freezable();
 
 	do {
-<<<<<<< HEAD
-=======
+
 		if (sbi->gc_mode == GC_URGENT_HIGH ||
 			!f2fs_available_free_memory(sbi, DISCARD_CACHE))
 			__init_discard_policy(sbi, &dpolicy, DPOLICY_FORCE, 1);
@@ -1704,7 +1703,6 @@ static int issue_discard_thread(void *data)
 		if (!atomic_read(&dcc->discard_cmd_cnt))
 			wait_ms = dpolicy.max_interval;
 
->>>>>>> upstream/android16-base
 		wait_event_interruptible_timeout(*q,
 				kthread_should_stop() || freezing(current) ||
 				dcc->discard_wake,
